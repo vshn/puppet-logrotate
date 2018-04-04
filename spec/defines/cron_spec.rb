538d595376c8b5da27fd15c4ef132cd6519d89aa
Rurik Yla-Onnenvuori <rurik.ylae-onnenvuori@baloise.com>
2018-04-04 09:42:34 +0200

    Improve test set to cover all supported os.
    FreeBSD is not in the list of supported os as the module has only
    partial support for it. The test set has some additional test cases to cover
    the differences in FreeBSD startup script.

--- spec/defines/cron_spec.rb
+++ spec/defines/cron_spec.rb
@@ -1,43 +1,83 @@
 require 'spec_helper'
 
 describe 'logrotate::cron' do
-  _, facts = on_supported_os.first
-  let(:facts) { facts }
-  let(:pre_condition) { 'class {"::logrotate": }' }
+  context 'supported operating systems' do
+    let(:pre_condition) { 'class { "::logrotate": }' }
 
-  context 'Default params' do
-    let(:title) { 'test' }
-    let(:params) { { ensure: 'present' } }
-    let(:facts) { { osfamily: 'RedHat' } }
+    on_supported_os.each do |os, facts|
+      context "on #{os}" do
+        let(:facts) do
+          facts
+        end
 
-    it {
-      is_expected.to contain_file('/etc/cron.test/logrotate').
-        with_ensure('present').
-        with_content(%r{(\/usr\/sbin\/logrotate \/etc\/logrotate.conf 2>&1)})
-    }
-  end
+        context 'With default params' do
+          let(:title) { 'test' }
+          let(:params) { { ensure: 'present' } }
 
-  context 'Default params (FreeBSD)' do
-    let(:title) { 'test' }
-    let(:params) { { ensure: 'present' } }
-    let(:facts) { { osfamily: 'FreeBSD' } }
+          if facts[:osfamily] != 'FreeBSD'
+            it {
+              is_expected.to contain_file('/etc/cron.test/logrotate').
+                with_ensure('present').
+                with_content(%r{(\/usr\/sbin\/logrotate \/etc\/logrotate.conf 2>&1)})
+            }
+          else
+            it {
+              is_expected.to contain_file('/usr/local/bin/logrotate.test.sh').
+                with_ensure('present').
+                with_content(%r{(\/usr\/local\/sbin\/logrotate \/usr\/local\/etc\/logrotate.conf 2>&1)})
+            }
+          end
+        end
 
-    it {
-      is_expected.to contain_file('/usr/local/bin/logrotate.test.sh').
-        with_ensure('present').
-        with_content(%r{(\/usr\/local\/sbin\/logrotate \/usr\/local\/etc\/logrotate.conf 2>&1)})
-    }
-  end
+        context 'With additional arguments' do
+          let(:pre_condition) { 'class {"::logrotate": logrotate_args => ["-s /var/lib/logrotate/logrotate.status", "-m /usr/sbin/mailer"]}' }
+          let(:title) { 'test' }
+          let(:params) { { ensure: 'present' } }
 
-  context 'With additional arguments' do
-    let(:pre_condition) { 'class {"::logrotate": logrotate_args => ["-s /var/lib/logrotate/logrotate.status", "-m /usr/sbin/mailer"]}' }
-    let(:title) { 'test' }
-    let(:params) { { ensure: 'present' } }
-    let(:facts) { { osfamily: 'RedHat' } }
+          if facts[:osfamily] != 'FreeBSD'
+            it {
+              is_expected.to contain_file('/etc/cron.test/logrotate').
+                with_ensure('present').
+                with_content(%r{(\/usr\/sbin\/logrotate -s \/var\/lib\/logrotate\/logrotate.status -m \/usr\/sbin\/mailer \/etc\/logrotate.conf 2>&1)})
+            }
+          else
+            it {
+              is_expected.to contain_file('/usr/local/bin/logrotate.test.sh').
+                with_ensure('present').
+                with_content(%r{(\/usr\/local\/sbin\/logrotate -s \/var\/lib\/logrotate\/logrotate.status -m \/usr\/sbin\/mailer \/usr\/local\/etc\/logrotate.conf 2>&1)})
+            }
+          end
+        end
+      end
+    end
 
-    it {
-      is_expected.to contain_file('/etc/cron.test/logrotate').
-        with_content(%r{(\/usr\/sbin\/logrotate -s \/var\/lib\/logrotate\/logrotate.status -m \/usr\/sbin\/mailer \/etc\/logrotate.conf 2>&1)})
-    }
+    # Test FreeBSD separately as it is only partially supported by the module and not in the list of supported os.
+    # When FreeBSD is added to the list of supported systems, these tests can be removed as they are already part of the test set above.
+    context 'on FreeBDS' do
+      let(:facts) { { osfamily: 'FreeBSD' } }
+
+      context 'With default params' do
+        let(:title) { 'test' }
+        let(:params) { { ensure: 'present' } }
+
+        it {
+          is_expected.to contain_file('/usr/local/bin/logrotate.test.sh').
+            with_ensure('present').
+            with_content(%r{(\/usr\/local\/sbin\/logrotate \/usr\/local\/etc\/logrotate.conf 2>&1)})
+        }
+      end
+
+      context 'With additional arguments' do
+        let(:pre_condition) { 'class {"::logrotate": logrotate_args => ["-s /var/lib/logrotate/logrotate.status", "-m /usr/sbin/mailer"]}' }
+        let(:title) { 'test' }
+        let(:params) { { ensure: 'present' } }
+
+        it {
+          is_expected.to contain_file('/usr/local/bin/logrotate.test.sh').
+            with_ensure('present').
+            with_content(%r{(\/usr\/local\/sbin\/logrotate -s \/var\/lib\/logrotate\/logrotate.status -m \/usr\/sbin\/mailer \/usr\/local\/etc\/logrotate.conf 2>&1)})
+        }
+      end
+    end
   end
 end
